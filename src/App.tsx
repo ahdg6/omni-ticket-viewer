e489f047972e1932760952b228eaff135c05ae1a
Misaka-L <lipww1234@foxmail.com>
2024-07-14 22:32:16 +0800

    fix: layout

--- src/App.tsx
+++ src/App.tsx
@@ -32,65 +32,63 @@ function App() {
   return (
     <ThemeContext.Provider value={theme}>
       <Layout className="bg-semi-color-bg-0 text-semi-color-text-0 h-screen overflow-hidden">
-        <Layout.Content className="flex flex-col">
-          <div className="flex flex-1">
-            <div className="w-96 p-5 border-r-2 border-semi-color-border">
+        <Layout.Content className="flex">
+          <div className="w-72 p-5 border-r-2 border-semi-color-border">
+            <Card.Meta
+              title="服务器名称"
+              description="🆘 问题求助"
+              className="mb-2"
+            />
+            <TicketTimeline ticket={ticket} />
+          </div>
+          <div className="flex flex-1 flex-col">
+            <div className="flex p-5 border-b-2 border-semi-color-border">
               <Card.Meta
-                title="服务器名称"
-                description="🆘 问题求助"
-                className="mb-2"
-              />
-              <TicketTimeline ticket={ticket} />
-            </div>
-            <div className="flex flex-col">
-              <div className="flex p-5 border-b-2 border-semi-color-border">
-                <Card.Meta
-                  className="flex-1"
-                  title="# 工单主题"
-                  description={
-                    <>
-                      <div className="flex gap-2">
-                        由
-                        <div className="flex">
-                          <Avatar
-                            src={asker.avatarUrl}
-                            size="extra-extra-small"
-                          />
-                          <span className="ml-1 font-semibold">
-                            {asker.name}
-                          </span>
-                        </div>
-                        发起，持续 {ticket.parameters.duration}
+                className="flex-1"
+                title="# 工单主题"
+                description={
+                  <>
+                    <div className="flex gap-2">
+                      由
+                      <div className="flex">
+                        <Avatar
+                          src={asker.avatarUrl}
+                          size="extra-extra-small"
+                        />
+                        <span className="ml-1 font-semibold">{asker.name}</span>
                       </div>
-                    </>
+                      发起，持续 {ticket.parameters.duration}
+                    </div>
+                  </>
+                }
+              />
+              <div className="flex">
+                <Button
+                  theme="borderless"
+                  type="tertiary"
+                  size="large"
+                  icon={theme === "dark" ? <IconMoon /> : <IconSun />}
+                  onClick={() =>
+                    updateTheme(theme === "dark" ? "light" : "dark")
                   }
                 />
-                <div className="flex">
-                  <Button
-                    theme="borderless"
-                    type="tertiary"
-                    size="large"
-                    icon={theme === "dark" ? <IconMoon /> : <IconSun />}
-                    onClick={() =>
-                      updateTheme(theme === "dark" ? "light" : "dark")
-                    }
-                  />
-                  <Button
-                    theme="borderless"
-                    type="tertiary"
-                    size="large"
-                    icon={<IconCopy />}
-                  />
+                <Button
+                  theme="borderless"
+                  type="tertiary"
+                  size="large"
+                  icon={<IconCopy />}
+                />
+              </div>
+            </div>
+            <div className="flex flex-1">
+              <div className="flex flex-1 flex-col h-full">
+                <div className="overflow-y-scroll p-4 flex-grow h-0">
+                  <MessageList ticket={ticket} />
                 </div>
               </div>
-              <div className="flex flex-1">
-                <div className="flex flex-col h-full">
-                  <div className="overflow-y-scroll p-4 flex-grow h-0">
-                    <MessageList ticket={ticket} />
-                  </div>
-                </div>
-                <div className="w-96 p-4 space-y-2 border-l-2 border-semi-color-border">
-                  <Card.Meta title="参与者" />
+              <div className="flex flex-col w-72 p-4 space-y-2 border-l-2 border-semi-color-border">
+                <Card.Meta title="参与者" />
+                <div className="space-y-2 flex-grow h-0 overflow-y-scroll">
                   <TicketParticipants ticket={ticket} />
                 </div>
               </div>
